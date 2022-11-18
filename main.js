b38bde4b754b359ba9265750256998ea074d40df
youryeet <85963171+youryeet@users.noreply.github.com>
2022-11-18 16:56:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("white");
+}
